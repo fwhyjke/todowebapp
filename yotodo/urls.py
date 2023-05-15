e8919e448c2221a90a1c1c744e147645f6c74b0d
fwhy <timofeevila423@gmail.com>
2023-05-15 19:47:47 +0700

    version 1.1

--- yotodo/urls.py
+++ yotodo/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('welcome', welcomepage, name='welcome'),
+    path('', welcomepage, name='welcome'),
     path('registration', Registration.as_view(), name='registration'),
     path('login', Login.as_view(), name='login'),
     path('logout', logout_user, name='logout'),
-    path('', MainPage.as_view(), name='main'),
+    path('main', MainPage.as_view(), name='main'),
     path('complete_task/<int:pk>/', CompleteTaskView.as_view(), name='complete_task'),
     path('fixed_task/<int:pk>/', FixedTaskView.as_view(), name='fixed_task'),
     path('unfixed_task/<int:pk>/', UnFixedTaskView.as_view(), name='unfixed_task'),
