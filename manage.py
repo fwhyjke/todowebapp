34d02461410068f4a921e60d27290d7202739dde
fwhy <timofeevila423@gmail.com>
2023-05-15 17:40:47 +0700

    setting.py update to use db with railway app env

--- manage.py
+++ manage.py
@@ -2,11 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import yotodoapp.yotodoapp.settings
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YotodoConfig.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yotodoapp.yotodoapp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
