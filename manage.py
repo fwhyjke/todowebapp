78ee9a42dee752f98597add2dc7e3a8ead4ff9ec
fwhy <timofeevila423@gmail.com>
2023-05-15 17:19:44 +0700

    add manage.py to raileay app

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yotodo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'railway.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
