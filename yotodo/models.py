d1ca5ea71f01e81d982ae6f4370148bc42f5bb0e
fwhy <timofeevila423@gmail.com>
2023-06-18 04:17:43 +0700

    Added connection between tasks and users table in Many to One mode

--- yotodo/models.py
+++ yotodo/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.utils import timezone
 from django.db import models
 from django.db.models import *
@@ -7,14 +8,14 @@ from django.urls import reverse
 
 
 class ToDoIt(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = IntegerField()
+    user = ForeignKey(User, on_delete=CASCADE)
+    created_at = DateTimeField(auto_now_add=True)
     title = CharField(max_length=30, verbose_name='Задача')
     lvl = IntegerField(verbose_name='Уровень')
     description = TextField(blank=True, verbose_name='Описание')
     status = BooleanField(default=False)
     isfixed = BooleanField()
-    end_at = models.DateTimeField(default=timezone.now)
+    end_at = DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f'User {self.user}, task "{self.title}"'
